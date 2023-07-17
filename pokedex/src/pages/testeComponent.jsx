b6f237f18f367c2949205d6a4829ee6fec23a2d6
Julia Santana <santana.j@ufba.br>
2023-07-17 15:07:16 -0300

    add login component resposiveness

--- pokedex/src/pages/testeComponent.jsx
+++ pokedex/src/pages/testeComponent.jsx
@@ -4,7 +4,6 @@ import Login from "@/components/loginModal/login"
 export default function login(){
     return(
         <div className={styles.wrapper}>
-            <h2>Teste de componente</h2>
             <Login></Login>
         </div>
     )
